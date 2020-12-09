f6abcc9c60bce38411161ed390bbba92ea79d0eb
Parn <parndepu@gmail.com>
2020-12-09 17:41:09 -0500

    fix adding map

--- code/frontend/js/map.js
+++ code/frontend/js/map.js
@@ -3,16 +3,15 @@
 ////////////////////////////////////////////////////////////////////////////////
 export var map_main = undefined;
 
+//
+var access_token = 'pk.eyJ1IjoiZGlnaXRhbGtpIiwiYSI6ImNqNXh1MDdibTA4bTMycnAweDBxYXBpYncifQ.daSatfva2eG-95QHWC9Mig';
+
 export function map_initialize(container_id) {
-
-    mapboxgl.access_token = 'pk.eyJ1IjoiZGlnaXRhbGtpIiwiYSI6ImNqNXh1MDdibTA4bTMycnAweDBxYXBpYncifQ.daSatfva2eG-95QHWC9Mig';
-
+    mapboxgl.access_token = access_token;
     map_main = new mapboxgl.Map({
         container: container_id,
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-74.5, 40],
         zoom: 9
     });
-
-    return;
 }
