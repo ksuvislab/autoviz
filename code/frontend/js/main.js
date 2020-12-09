73dc01ac63cc9810bbe81e077ce7d7a77cfb0fd4
Parn <parndepu@gmail.com>
2020-12-09 17:26:50 -0500

    test adding mapbox

---
+++ code/frontend/js/main.js
@@ -0,0 +1,25 @@
+import {
+    map_initialize,
+    mapbox_map
+} from './map.js';
+
+////////////////////////////////////////////////////////////////////////////////
+// Initialize
+////////////////////////////////////////////////////////////////////////////////
+draw_map();
+
+// Testing
+console.log(d3);
+console.log($);
+console.log(jQuery);
+
+function draw_map() {
+    let map_container = document.createElement('div');
+    map_container.setAttribute('id', 'map');
+    map_container.style.width = '400px';
+    map_container.style.height = '300px';
+    document.body.appendChild(map_container);
+    map_initialize('map');
+    console.log(mapbox_map);
+    return;
+}
---
+++ code/frontend/js/map.js
@@ -0,0 +1,15 @@
+mapboxgl.access_token = 'pk.eyJ1IjoiZGlnaXRhbGtpIiwiYSI6ImNqNXh1MDdibTA4bTMycnAweDBxYXBpYncifQ.daSatfva2eG-95QHWC9Mig';
+
+////////////////////////////////////////////////////////////////////////////////
+//  Global Variables
+////////////////////////////////////////////////////////////////////////////////
+mapbox_map = undefined;
+
+function map_initialize(container_id) {
+    mapbox_map = new mapboxgl.Map({
+        container: container_id,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-74.5, 40],
+        zoom: 9
+    });
+}
