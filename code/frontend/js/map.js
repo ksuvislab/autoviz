ea333775893431195c036503bc191a33ed384e1b
Parn <parndepu@gmail.com>
2020-12-09 17:38:14 -0500

    fix adding map

--- code/frontend/js/map.js
+++ code/frontend/js/map.js
@@ -1,15 +1,18 @@
-mapboxgl.access_token = 'pk.eyJ1IjoiZGlnaXRhbGtpIiwiYSI6ImNqNXh1MDdibTA4bTMycnAweDBxYXBpYncifQ.daSatfva2eG-95QHWC9Mig';
-
 ////////////////////////////////////////////////////////////////////////////////
 //  Global Variables
 ////////////////////////////////////////////////////////////////////////////////
 export var map_main = undefined;
 
 export function map_initialize(container_id) {
+
+    mapboxgl.access_token = 'pk.eyJ1IjoiZGlnaXRhbGtpIiwiYSI6ImNqNXh1MDdibTA4bTMycnAweDBxYXBpYncifQ.daSatfva2eG-95QHWC9Mig';
+
     map_main = new mapboxgl.Map({
         container: container_id,
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-74.5, 40],
         zoom: 9
     });
+
+    return;
 }
